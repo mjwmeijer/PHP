883d5421a5eadd6054fd715d1402198ccaa16179
mjwmeijer <mjwmeijer@yahoo.com>
2017-11-10 18:17:12 +0100

    First php file.

---
+++ echo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Handboek</title>
+</head>
+
+<body>
+  <?php
+  $txt1 = "<h2>My name is </h2>"
+  echo . $txt1 . "Martin";
+
+   ?>
+
+
+</body>
+
+</html>
